7e9c666bfd1bc9fdffdd8ac750985ae652c29915
Ian <spyguyian@gmail.com>
2023-02-12 12:06:09 -0500

    Moved to masking as suggested in the project

--- src.cpp
+++ src.cpp
@@ -15,18 +15,15 @@ std::vector<cv::Mat> ReadImages(std::string folder){
     std::vector<std::filesystem::path> entries;
 
     // Sort the files by numerical order.
-    for(const auto &entry : std::filesystem::directory_iterator(folder))
-    {
+    for(const auto &entry : std::filesystem::directory_iterator(folder)){
         entries.push_back(entry.path());
         sort(entries.begin(), entries.end(), CompareFilepaths);
     }
 
-    // Read in an order:
-    for(const auto &entry : entries)
-    {
-        // Print the file name
+    // Read in an order
+    for(const auto &entry : entries){
         cv::Mat image = cv::imread(entry.string());
-        cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
+        cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
         images.push_back(image);
     }
 
@@ -39,25 +36,28 @@ int main(int argc, char** argv)
     // Read in images
     std::vector<cv::Mat> images = ReadImages("Office");
 
-    // Convert to find temporal gradient
-    std::vector<cv::Mat> temporal_gradient;
-    for (int i = 0; i < images.size() - 1; i++)
-    {
+    // Convert to find temporal gradient mask
+    std::vector<cv::Mat> temporal_gradient_mask;
+    for (int i = 0; i < images.size() - 1; i++){
         cv::Mat temp_grad;
         cv::absdiff(images[i], images[i+1], temp_grad);
+
         // Thresholding
         cv::threshold(temp_grad, temp_grad, 50, 255, cv::THRESH_BINARY);
-        temporal_gradient.push_back(temp_grad);
+        temporal_gradient_mask.push_back(temp_grad);
     }
 
     // Show the images like a video and save it
-    cv::VideoWriter video("output.avi", cv::VideoWriter::fourcc('M','J','P','G'), 10, temporal_gradient[0].size());
-    int i = 0;
-    while (i < temporal_gradient.size())
-    {
-        cv::imshow("Video", temporal_gradient[i]);
-        cv::waitKey(25);
-        video.write(temporal_gradient[i]);
-        i++;
+    cv::VideoWriter video("output.avi", cv::VideoWriter::fourcc('M','J','P','G'), 10, temporal_gradient_mask[0].size());
+    for(int i = 0; i < images.size(); i++){
+        cv::Mat masked_img;
+
+        cv::bitwise_and(images[i], temporal_gradient_mask[i], masked_img);
+
+        cv::imshow("Video", masked_img);
+        if(cv::waitKey(25) == 'q')
+            break;
+
+        video.write(masked_img);
     }    
 }
