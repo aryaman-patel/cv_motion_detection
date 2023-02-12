ebc467cffb5ebfa49b84459cf81058718e4afea4
Ian <spyguyian@gmail.com>
2023-02-12 12:43:44 -0500

    Simple Prewitt-like filter added

--- src.cpp
+++ src.cpp
@@ -8,6 +8,7 @@ bool CompareFilepaths(std::filesystem::path a, std::filesystem::path b){
     return std::stoi(a.filename().string()) < std::stoi(b.filename().string());
 }
 
+
 /// Reads images in `folder` and returns them in a vector
 std::vector<cv::Mat> ReadImages(std::string folder){
     // Read images from the folder
@@ -30,12 +31,9 @@ std::vector<cv::Mat> ReadImages(std::string folder){
     return images;
 }
 
-/// main
-int main(int argc, char** argv)
-{
-    // Read in images
-    std::vector<cv::Mat> images = ReadImages("Office");
 
+/// Returns a set of masks that indicate thresholded temporal gradients
+std::vector<cv::Mat> TemporalGradientSimple(std::vector<cv::Mat> images, uint8_t thresh){
     // Convert to find temporal gradient mask
     std::vector<cv::Mat> temporal_gradient_mask;
     for (int i = 0; i < images.size() - 1; i++){
@@ -43,21 +41,70 @@ int main(int argc, char** argv)
         cv::absdiff(images[i], images[i+1], temp_grad);
 
         // Thresholding
-        cv::threshold(temp_grad, temp_grad, 50, 255, cv::THRESH_BINARY);
+        cv::threshold(temp_grad, temp_grad, thresh, 255, cv::THRESH_BINARY);
         temporal_gradient_mask.push_back(temp_grad);
     }
 
+    return temporal_gradient_mask;
+}
+
+
+/// Returns a set of masks that indicate thresholded temporal gradients using a 1x3 filter
+std::vector<cv::Mat> TemporalGradientFilter(std::vector<cv::Mat> images, uint8_t thresh){
+    // Convert to find temporal gradient mask
+    std::vector<cv::Mat> mask;
+    for (int i = 1; i < images.size() - 1; i++){
+        cv::Mat temp_grad;
+        cv::addWeighted(images[i-1], -0.5, images[i+1], 0.5, 0, temp_grad);
+
+        // Thresholding
+        cv::threshold(temp_grad, temp_grad, thresh, 255, cv::THRESH_BINARY);
+        mask.push_back(temp_grad);
+    }
+
+    return mask;
+}
+
+
+/// Returns a set of masks that indicate thresholded temporal gradients using a 1x3 filter
+std::vector<cv::Mat> TemporalGradientDoG(std::vector<cv::Mat> images, uint8_t thresh, float tsigma){
+    // TODO
+    std::vector<cv::Mat> mask;
+    return mask;
+}
+
+
+/// main
+int main(int argc, char** argv)
+{
+    // Read in images
+    std::vector<cv::Mat> images = ReadImages("Office");
+
+    // Get simple temporal derivative mask
+    std::vector<cv::Mat> simple_gradient_mask = TemporalGradientSimple(images, 10);
+
+    // Get filter temporal derivative mask
+    std::vector<cv::Mat> filter_gradient_mask = TemporalGradientFilter(images, 10);
+
     // Show the images like a video and save it
-    cv::VideoWriter video("output.avi", cv::VideoWriter::fourcc('M','J','P','G'), 10, temporal_gradient_mask[0].size());
-    for(int i = 0; i < images.size(); i++){
-        cv::Mat masked_img;
+    cv::VideoWriter video("output.avi", 
+                          cv::VideoWriter::fourcc('M','J','P','G'), 
+                          10, filter_gradient_mask[0].size());
+    for(int i = 0; i < filter_gradient_mask.size(); i++){
+        cv::Mat simple_grad_img, filter_grad_img, frame;
 
-        cv::bitwise_and(images[i], temporal_gradient_mask[i], masked_img);
+        // Apply masks
+        cv::bitwise_and(images[i], simple_gradient_mask[i], simple_grad_img);
+        cv::bitwise_and(images[i+1], filter_gradient_mask[i], filter_grad_img);
 
-        cv::imshow("Video", masked_img);
-        if(cv::waitKey(25) == 'q')
+        // Concatenate for viewing
+        cv::hconcat(images[i], simple_grad_img, frame);
+        cv::hconcat(frame, filter_grad_img, frame);
+
+        cv::imshow("Video", frame);
+        if(cv::waitKey(10) == 'q')
             break;
 
-        video.write(masked_img);
+        video.write(frame);
     }    
 }
